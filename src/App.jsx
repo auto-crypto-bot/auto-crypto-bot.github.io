afae0edad1df81b7fa9679c4148be8f68dfc866f
mzeeshan42 <mzeeshan6198@gmail.com>
2026-01-04 15:45:38 -0500

    fix: add basename to react router for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Settings from './pages/Settings';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
